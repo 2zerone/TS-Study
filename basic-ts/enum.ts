fbb5e7b247f52b135c62a31b87335feeb44dfb62
2zerone <dudgks3316@naver.com>
2023-10-20 15:27:58 +0900

    enum & literal

---
+++ basic-ts/enum.ts
@@ -0,0 +1,37 @@
+// enum
+enum categoryEnum {
+    H = "H",
+    K = "K",
+    S = "S",
+}
+
+const category: categoryEnum = categoryEnum.H;
+
+// web app
+// base line
+// server
+
+function cate(category: any) {
+    if(category === "H") {
+        console.log("5% 할인")
+    } else if(category === "K") {
+        console.log("50% 할인")
+    } else if(category === "S") {
+        console.log("80% 할인")
+    } else {
+        console.log("server stop")
+    }
+}
+
+cate(category);
+
+// literal type
+
+enum sexEnum {
+    MALE = "MALE",
+    FEMALE = "FEMALE",
+    ETC = "ETC",
+}
+
+const sex: sexEnum = sexEnum.MALE;
+const sex2: "MALE" | "FEMALE" | "ETC" = "MALE"; // 재활용하지 않고 값도 많지 않을 경우 사용
